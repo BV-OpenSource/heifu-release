3fa3d17418bd90a78401972229b08e4dcc9e3366
André <andre.ferreira@pdmfc.com>
2020-08-28 12:46:19 +0100

    Revert "Trimmed the branch to only the heifu_diagnostic sub directory"
    This reverts commit 1d011af1db7ac7b28e20184cf4fcc9fc8268420f.

---
+++ scripts/UseCase3.sh
@@ -0,0 +1,26 @@
+echo "Initializing Dummy Simulation!"
+
+gnome-terminal -x bash -c "source ~/heifu_ws/devel/setup.bash; roslaunch heifu_description gazebo.launch"
+
+sleep 5
+
+gnome-terminal -x bash -c "source ~/heifu_ws/devel/setup.bash; roslaunch heifu_mavros heifu_mavros.launch argID:=2 xi:=-30 Y:=-1.57079632676"
+ 
+sleep 5
+
+gnome-terminal -x bash -c "source ~/heifu_ws/devel/setup.bash; cd ~/ardupilot/ArduCopter; sim_vehicle.py -v ArduCopter -f gazebo-heifu -I2"
+
+sleep 5
+gnome-terminal -x bash -c "source ~/heifu_ws/devel/setup.bash; roslaunch heifu_sim heifu_sim.launch argID:=2"
+
+sleep 5
+
+gnome-terminal -x bash -c "source ~/heifu_ws/devel/setup.bash; roslaunch heifu_mavros heifu_mavros.launch argID:=1 yi:=-30"
+
+sleep 5
+
+gnome-terminal -x bash -c "source ~/heifu_ws/devel/setup.bash; cd ~/ardupilot/ArduCopter; sim_vehicle.py -v ArduCopter -f gazebo-heifu -I1"
+
+sleep 5
+
+gnome-terminal -x bash -c "source ~/heifu_ws/devel/setup.bash; roslaunch heifu_sim_sensors heifu_sim_sensors.launch argID:=1 argUseCase3:=1"
