6523bf7acc55f3b995be040b8760a12c21d1cf7c
André Filipe <andre.ferreira@beyond-vision.pt>
2020-12-16 18:20:03 +0000

    Trimmed the branch to only the heifu_simple_waypoint sub directory

--- heifu_interface/playAudio.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Call the running script with the received arguments
-python /home/heifu/heifu_ws/src/heifu/heifu_interface/playAudio.py "$@" &
-
-# Get value of --name from second set of arguments
-processFiles="/home/heifu/audios/process_file"
-echo $! >> "${processFiles}"
