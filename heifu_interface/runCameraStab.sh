72174ccd916aa1cf5e24e7bd60dbbd004f827f4a
André Filipe <andre.ferreira@beyond-vision.pt>
2020-12-17 11:12:10 +0000

    Trimmed the branch to only the heifu_msgs sub directory

--- heifu_interface/runCameraStab.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Call the running script with the received arguments
-python3 /home/heifu/heifu_ws/src/heifu/heifu_interface/cameraStab.py "$@" &
-
-# Get value of --name from second set of arguments
-processFiles="/home/heifu/heifu_ws/src/heifu/heifu_interface/process_file"
-echo $! >> "${processFiles}"
