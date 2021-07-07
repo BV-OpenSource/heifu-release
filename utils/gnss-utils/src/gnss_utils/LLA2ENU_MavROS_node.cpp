fe71ee34904ec06bb5593ae9467350a2e1d98d4a
André Filipe <andre.ferreira@beyond-vision.pt>
2021-07-07 12:32:03 +0100

    Trimmed the branch to only the control/navigation-controller sub directory

--- utils/gnss-utils/src/gnss_utils/LLA2ENU_MavROS_node.cpp
+++
@@ -1,9 +0,0 @@
-#include "gnss_utils/LLA2ENU_MavROS.hpp"
-
-int main(int argc, char **argv)
-{
-	ros::init(argc, argv, "LLA2ENU_MavROS_node");
-	LLA2ENU_MavROS LLA2ENU_MavROS_node;
-	LLA2ENU_MavROS_node.Run();
-	return 0;
-}
